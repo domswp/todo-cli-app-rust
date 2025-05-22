d33956f8e1c70a36cbf082943728498c6feb0aac
domswp <domaswiladatup@gmail.com>
2025-05-22 23:57:53 +0700

    update todos.txt

--- todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92.d
+++ todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92.d
@@ -1,6 +1,6 @@
-/home/domaswp/Documents/Program/rust/rust_todo_cli/todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92: src/main.rs src/task.rs src/storage.rs src/menu.rs
+/home/domaswp/Documents/program/rust/todo-cli-app-rust/todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92: src/main.rs src/task.rs src/storage.rs src/menu.rs
 
-/home/domaswp/Documents/Program/rust/rust_todo_cli/todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92.d: src/main.rs src/task.rs src/storage.rs src/menu.rs
+/home/domaswp/Documents/program/rust/todo-cli-app-rust/todo-cli/target/debug/deps/todo_cli-33e5f9c1e8e36a92.d: src/main.rs src/task.rs src/storage.rs src/menu.rs
 
 src/main.rs:
 src/task.rs:
